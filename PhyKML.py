6e09dee4525ab90e2712665a578b5c54a6dc3f2b
PAPALEOXV <papaleoxv@gmail.com>
2026-06-25 04:07:47 +0800

    Add files via upload

--- PhyKML.py
+++ PhyKML.py
@@ -5,11 +5,11 @@ kmln="example" #name
 mnr=5 #start row
 crd=[]
 for row in sht.iter_rows(min_row=mnr,values_only=True):
-    lat=row[1]
-    lon=row[2]
+    lat=row[1] #latitude
+    lon=row[2] #longitude
     if lat is not None and lon is not None:
         crd.append((float(lon),float(lat)))
-print(len(crd))
+print(len(crd),"coords")
 crds=' '.join([f'{lon},{lat}'for lon,lat in crd])
 kmls=f"""<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
