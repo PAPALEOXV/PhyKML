25ec5144f7ebf1c7f5fc97cd77e644fc0b0b44aa
PAPALEOXV <papaleoxv@gmail.com>
2026-07-10 00:16:20 +0800

    更新 PhyKML.py

--- PhyKML.py
+++ PhyKML.py
@@ -1,29 +1,28 @@
 import openpyxl
-wb=openpyxl.load_workbook('example.xlsx') #file
-sht=wb['Raw Data'] #sheet
-kmln="example" #name
-srw=5 #start row
-crd=[]
-for row in sht.iter_rows(min_row=srw,values_only=True):
-    lat=row[1] #latitude
-    lon=row[2] #longitude
-    if lat is not None and lon is not None:
-        crd.append((float(lon),float(lat)))
-print(len(crd),"coords")
-crds=' '.join([f'{lon},{lat}'for lon,lat in crd])
+workbook=openpyxl.load_workbook('exemple.xls')
+sheet=workbook['Raw Data']
+kml_name="exemple"
+start_row=5
+coord=[]
+for row in sheet.iter_rows(min_row=start_row,values_only=True):
+    lat=row[1]
+    lon=row[2]
+    if lat is not None and lon is not None:        coord.append((float(lon),float(lat)))
+print(len(coord),"coords")
+coords=' '.join([f'{lon},{lat}'for lon,lat in coord])
 kml=f"""<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
   <Document>
-    <name>{kmln}</name>
+    <name>{kml_name}</name>
     <Placemark>
-      <name>{kmln}</name>
+      <name>{kml_name}</name>
       <LineString>
-        <coordinates>{crds}</coordinates>
+        <coordinates>{coords}</coordinates>
       </LineString>
     </Placemark>
   </Document>
 </kml>
 """
-with open(f'{kmln}.kml','w',encoding='utf-8')as fil:
-    fil.write(kml)
+with open(f'{kml_name}.kml','w',encoding='utf-8')as out_file:
+    out_file.write(kml)
 print("OK")
