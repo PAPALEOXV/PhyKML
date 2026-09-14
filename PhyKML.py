2a12543328ba35b6256ced7a1766c0db4cf57cb8
PAPALEOXV <papaleoxv@gmail.com>
2026-09-15 01:55:07 +0800

    更新 PhyKML.py

--- PhyKML.py
+++ PhyKML.py
@@ -1,36 +1,33 @@
-import openpyxl
-xlsx=input()
-sheet=input()
+import csv
 name=input()
-start=int(input())
-xlsx=openpyxl.load_workbook(xlsx)
-sheet=xlsx[sheet]
-coord=[]
-for row in sheet.iter_rows(min_row=start,values_only=True):
-    lon=row[2]
-    lat=row[1]
-    if lat is not None and lon is not None:
-        coord.append((float(lon),float(lat)))
-print(len(coord))
-data=' '.join([f'{lon},{lat}'for lon,lat in coord])
-data=f'''<?xml version="1.0" encoding="UTF-8"?>
+data=[]
+with open(name+'.csv',mode='r',encoding='utf-8-sig')as file:
+    file=csv.reader(file)
+    next(file)
+    for row in file:
+        lon=row[2]
+        lat=row[1]
+        data.append(lon+','+lat)
+print(len(data))
+data=' '.join(data)
+data='''<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
     <Document>
-        <name>{name}</name>
+        <name>'''+name+'''</name>
         <Placemark>
-        <name>{name}</name>
-        <Style>
-            <LineStyle>
-                <color>ff2dc0fb</color>
-                <width>1.33</width>
-            </LineStyle>
-        </Style>
-        <LineString>
-            <coordinates>{data}</coordinates>
-        </LineString>
+            <name>'''+name+'''</name>
+            <Style>
+                <LineStyle>
+                    <color>ff2dc0fb</color>
+                    <width>1.33</width>
+                </LineStyle>
+            </Style>
+            <LineString>
+                <coordinates>'''+data+'''</coordinates>
+            </LineString>
         </Placemark>
     </Document>
 </kml>'''
-with open(f'{name}.kml','w',encoding='utf-8')as kml:
-    kml.write(data)
-print(f'{name}.kml')
+with open(name+'.kml',mode='w',encoding='utf-8')as file:
+    file.write(data)
+print(name+'.kml')
